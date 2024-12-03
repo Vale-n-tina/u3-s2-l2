99af406fad1d41e3cbec3ca3af64ac87b37a55c4
Vale-n-tina <valentia.grg@gmail.com>
2024-12-03 17:13:46 +0100

    .

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -5,10 +5,9 @@ import Loading from "./Loading";
 import Error from "./Error";
 
 const CommentArea = (props) => {
-  
-  const [comments,setcomments]=useState([])
-  const [ isLoading,setisLoading]=useState(false)
-  const [ isError, setisError]=useState(false)
+  const [comments, setcomments] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  const [isError, setisError] = useState(false);
 
   // componentDidMount = async () => {
   //   try {
@@ -35,56 +34,46 @@ const CommentArea = (props) => {
   //     this.setState({ isLoading: false, isError: true })
   //   }
   // }
-    
-  
 
-  const funzione= async () => {      
-    
-      try {
-        let response = await fetch(
-          "https://striveschool-api.herokuapp.com/api/comments/" +
-            props.asin,
-          {
-            headers: {
-              Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzQ4NmYwNTA2ZmM4YzAwMTU2Yjg2ZmIiLCJpYXQiOjE3MzI4MDAyNjIsImV4cCI6MTczNDAwOTg2Mn0.LBzHQxm8Ovl76SSc6dJv3F12CSJnOAhnNlzvhQB2oec",
-            },
-          }
-        );
-        console.log(response);
-        if (response.ok) {
-          let comments = await response.json();
-         
-            setcomments(comments)
-            setisLoading(false)
-            setisError(false)
-          
-        } else {
-          setisLoading(false)
-          setisError(true)
-          
+  const funzione = async () => {
+    try {
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/comments/" + props.asin,
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzQ4NmYwNTA2ZmM4YzAwMTU2Yjg2ZmIiLCJpYXQiOjE3MzI4MDAyNjIsImV4cCI6MTczNDAwOTg2Mn0.LBzHQxm8Ovl76SSc6dJv3F12CSJnOAhnNlzvhQB2oec",
+          },
         }
-      } catch (error) {
-        console.log(error);
-        setisLoading(false)
-        setisError(true)
-        
-      }
-    
-  };
-  useEffect(()=>{
-    if(props.asin !== null){
-      funzione()
-    }
-    
+      );
+      console.log(response);
+      if (response.ok) {
+        let comments = await response.json();
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[props.asin] )   
- 
+        setcomments(comments);
+        setisLoading(false);
+        setisError(false);
+      } else {
+        setisLoading(false);
+        setisError(true);
+      }
+    } catch (error) {
+      console.log(error);
+      setisLoading(false);
+      setisError(true);
+    }
+  };
+  useEffect(() => {
+    if (props.asin !== null) {
+      funzione();
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.asin]);
+
   return (
     <div className="text-center">
-      {
-      isLoading && <Loading />}
+      {isLoading && <Loading />}
       {isError && <Error />}
       <AddComment asin={props.asin} />
       <CommentList commentsToShow={comments} />
