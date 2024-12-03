f5e2cc8b5cab3b9d20a82d74db76d4e968a7cee7
Vale-n-tina <valentia.grg@gmail.com>
2024-12-03 16:27:12 +0100

    .

---
+++ src/components/AddComment.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
+const AddComment = (props) => {
+  const [comment, setComment] = useState({
+    comment: "",
+    rate: 1,
+    elementId: props.asin,
+  });
+
+  useEffect(() => {
+    setComment({ ...comment, elementId: props.asin });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.asin]);
+
+  const sendComment = async (e) => {
+    e.preventDefault();
+    try {
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/comments",
+        {
+          method: "POST",
+          body: JSON.stringify(comment),
+          headers: {
+            "Content-type": "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzQ4NmYwNTA2ZmM4YzAwMTU2Yjg2ZmIiLCJpYXQiOjE3MzI4MDAyNjIsImV4cCI6MTczNDAwOTg2Mn0.LBzHQxm8Ovl76SSc6dJv3F12CSJnOAhnNlzvhQB2oec",
+          },
+        }
+      );
+      if (response.ok) {
+        alert("Recensione inviata!");
+        setComment({ comment: "", rate: 1, elementId: props.asin });
+      } else {
+        throw new Error("Qualcosa è andato storto");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return (
+    <div className="my-3">
+      <Form onSubmit={sendComment}>
+        <Form.Group className="mb-2">
+          <Form.Label>Recensione</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Inserisci qui il testo"
+            value={comment.comment}
+            onChange={(e) =>
+              setComment({...comment, comment:e.target.value})
+              
+            }
+          />
+        </Form.Group>
+        <Form.Group className="mb-2">
+          <Form.Label>Valutazione</Form.Label>
+          <Form.Control
+            as="select"
+            value={comment.rate}
+            onChange={(e) =>
+              setComment({...comment, rate:e.target.value})
+              
+            }
+          >
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </Form.Control>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Invia
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default AddComment;
---
+++ src/components/CommentArea.jsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from "react";
+import CommentList from "./CommentList";
+import AddComment from "./AddComment";
+import Loading from "./Loading";
+import Error from "./Error";
+
+const CommentArea = (props) => {
+  
+  const [comments,setcomments]=useState([])
+  const [ isLoading,setisLoading]=useState(false)
+  const [ isError, setisError]=useState(false)
+
+  // componentDidMount = async () => {
+  //   try {
+  //     let response = await fetch(
+  //       'https://striveschool-api.herokuapp.com/api/comments/' +
+  //         this.props.asin,
+  //       {
+  //         headers: {
+  //           Authorization:
+  //             'Bearer inserisci-qui-il-tuo-token',
+  //         },
+  //       }
+  //     )
+  //     console.log(response)
+  //     if (response.ok) {
+  //       let comments = await response.json()
+  //       this.setState({ comments: comments, isLoading: false, isError: false })
+  //     } else {
+  //       console.log('error')
+  //       this.setState({ isLoading: false, isError: true })
+  //     }
+  //   } catch (error) {
+  //     console.log(error)
+  //     this.setState({ isLoading: false, isError: true })
+  //   }
+  // }
+    
+  
+
+  const funzione= async () => {      
+    
+      try {
+        let response = await fetch(
+          "https://striveschool-api.herokuapp.com/api/comments/" +
+            props.asin,
+          {
+            headers: {
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzQ4NmYwNTA2ZmM4YzAwMTU2Yjg2ZmIiLCJpYXQiOjE3MzI4MDAyNjIsImV4cCI6MTczNDAwOTg2Mn0.LBzHQxm8Ovl76SSc6dJv3F12CSJnOAhnNlzvhQB2oec",
+            },
+          }
+        );
+        console.log(response);
+        if (response.ok) {
+          let comments = await response.json();
+         
+            setcomments(comments)
+            setisLoading(false)
+            setisError(false)
+          
+        } else {
+          setisLoading(false)
+          setisError(true)
+          
+        }
+      } catch (error) {
+        console.log(error);
+        setisLoading(false)
+        setisError(true)
+        
+      }
+    
+  };
+  useEffect(()=>{
+    funzione()
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[props.asin] )   
+ 
+  return (
+    <div className="text-center">
+      {
+      isLoading && <Loading />}
+      {isError && <Error />}
+      <AddComment asin={props.asin} />
+      <CommentList commentsToShow={comments} />
+    </div>
+  );
+};
+
+export default CommentArea;
